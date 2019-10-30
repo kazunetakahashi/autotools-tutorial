acd7fd3febd4b07970735ed65e3355c2871e7829
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-10-30 10:24:15 +0900

    initial commit

---
+++ src/main.c
@@ -0,0 +1,16 @@
+/**
+ * File    : main.c
+ * Author  : Kazune Takahashi
+ * Created : 2019/10/30 9:45:04
+ * Powered by Visual Studio Code
+ */
+
+#include <config.h>
+#include <stdio.h>
+
+int main()
+{
+  puts("Hello, World!");
+  puts("This is " PACKAGE_STRING ".");
+  return 0;
+}
