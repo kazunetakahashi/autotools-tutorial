69e929a393e7ce0cbb587798ec30cf2ca3114540
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-10-30 12:00:36 +0900

    lice

--- src/main.c
+++ src/main.c
@@ -1,3 +1,12 @@
+/*
+ * autotools-tutorial (c) 2019 by Kazune Takahashi
+ *
+ * autotools-tutorial is licensed under a
+ * Creative Commons Attribution-ShareAlike 3.0 Unported License.
+ *
+ * You should have received a copy of the license along with this
+ * work.  If not, see <http://creativecommons.org/licenses/by-sa/3.0/>. */
+
 /**
  * File    : main.c
  * Author  : Kazune Takahashi
